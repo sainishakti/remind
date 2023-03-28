cc4c6b29faf0724228ab3a9e01506fa1dc713449
shakti <shakti.ingenioushitech@gmail.com>
2023-03-28 20:29:39 +0530

    initial commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,14 @@
 module.exports = {
-    HOST:   "192.168.1.22",
+    // HOST:   '127.0.0.1',
+    // USER: 'root',
+    // PASSWORD: 'password@123',
+    // DB: 'remind',
+    // dialect: 'mysql',
+    HOST:   "cb@us-cdbr-east-06.cleardb.net",
     USER: "bb369c3b7e8a64",
     PASSWORD: "8d33d3cb",
     DB: "heroku_fa7e24f6f61ebe3",
     dialect: "mysql",
-   
 
     pool: {
         max: 5,
